dce6153119cdf7eda5fa4a286503160c45138b04
rboutin29 <69016629+rboutin29@users.noreply.github.com>
2021-01-13 14:23:17 -0500

    Update settings.py

--- edunet_site/edunet_site/settings.py
+++ edunet_site/edunet_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iefy%@nm&k$+^mlp1_50kpn6m!npw3*$#a(g4pwt5@%vkk7-c_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
