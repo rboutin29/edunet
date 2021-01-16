5ad9aa017af90a12f6511707d14e90ca53c9314a
rboutin29 <rian.boutin@student.fairfield.edu>
2021-01-15 19:12:36 -0500

    new db

--- edunet_site/settings.py
+++ edunet_site/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
+            'timeout': 20,  # in seconds
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
